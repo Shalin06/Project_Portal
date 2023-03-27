d071a916784fd1fcf928bad1157a5139459f8733
yogeshjangir16 <7b148yogesh@gmail.com>
2023-03-27 22:16:23 +0530

    images folder changed
    Some images has been added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,14 +5,14 @@ import { Button } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 import { useUserAuth } from "../context/UserAuthContext";
 import { database } from "../firebase";
-import {ref, onValue} from "firebase/database";
+import { ref, onValue } from "firebase/database";
 import { auth } from "../firebase";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const { logIn, googleSignIn} = useUserAuth();
+  const { logIn, googleSignIn } = useUserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -25,103 +25,109 @@ const Login = () => {
         const data_ref = ref(database, "users/" + user.uid);
         onValue(data_ref, (snapshot) => {
           if (snapshot.exists()) {
-            if(snapshot.val().Profession === "Student"){
+            if (snapshot.val().Profession === "Student") {
               navigate("/studenthome");
             }
-            else if(snapshot.val().Profession === "Professor"){
+            else if (snapshot.val().Profession === "Professor") {
               navigate("/profhome")
             }
             else {
               navigate("/details");
             }
-          } 
+          }
           else {
             navigate("/details");
           }
         });
-    } else {
-      // handle case where user is null or undefined
-      console.log("User is null or undefined");
+      } else {
+        // handle case where user is null or undefined
+        console.log("User is null or undefined");
+      }
+    } catch (error) {
+      console.log(error.message);
     }
-  } catch (error) {
-    console.log(error.message);
-  }
   };
 
   const handleGoogleSignIn = async (e) => {
-    e.preventDefault(); 
-  try {
-    await googleSignIn();
-    const user = auth.currentUser;
-    if (user) {
-      const data_ref = ref(database, "users/" + user.uid);
-      onValue(data_ref, (snapshot) => {
-        if (snapshot.exists()) {
-          if(snapshot.val().Profession === "Student"){
-            navigate("/studenthome");
+    e.preventDefault();
+    try {
+      await googleSignIn();
+      const user = auth.currentUser;
+      if (user) {
+        const data_ref = ref(database, "users/" + user.uid);
+        onValue(data_ref, (snapshot) => {
+          if (snapshot.exists()) {
+            if (snapshot.val().Profession === "Student") {
+              navigate("/studenthome");
+            }
+            else {
+              navigate("/profhome")
+            }
+          } else {
+            navigate("/details");
           }
-          else{
-            navigate("/profhome")
-          }
-        } else {
-          navigate("/details");
-        }
-      });
-    } else {
-      // handle case where user is null or undefined
-      console.log("User is null or undefined");
+        });
+      } else {
+        // handle case where user is null or undefined
+        console.log("User is null or undefined");
+      }
+    } catch (error) {
+      console.log(error.message);
     }
-  } catch (error) {
-    console.log(error.message);
-  }
   };
 
   return (
-    
+
     <>
-    {/* <div>
-      <img src="Images\WhatsApp Image 2023-03-09 at 18.15.05.jpg"></img>
-    </div> */}
-      <div>
-        <p className="Title">INDIAN INSTITUTE OF TECHNOLOGY JODHPUR</p>
-      </div>
-      <div className="container">
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit" className="LoginButton">
-              Log In
-            </Button>
+      <body>
+        <div className="container">
+          <div className="login_navbar">
+            <img src={'Images/final_logo.png'} className="logo" alt="It's is not loding"/>
+            <nav>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <div className="form-inline">
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Control className="login_input"
+                      type="email"
+                      placeholder="Email address"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Control className="login_input"
+                      type="password"
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Form.Group>
+                  <div>
+                    <GoogleButton
+                      className="g-btn"
+                      type="dark"
+                      onClick={handleGoogleSignIn}
+                    />
+                    </div>
+                  <Button variant="primary" type="Submit" className="LoginButton">
+                    Login
+                  </Button>
+                </div>
+              </Form>
+            </nav>
           </div>
-        </Form>
-        <hr />
-        <div>
-          <GoogleButton
-            className="g-btn"
-            type="dark"
-            onClick={handleGoogleSignIn}
-          />
+          <div className="row">
+            <div className="col-1">
+              <h2>Boost up your skills</h2>
+              <h3>with new ways of</h3>
+              <h4>learning</h4>
+              <button type = "button" className="btn1">Apply</button>
+            </div>
+            <div className="col-2">
+              <img src = "images/back22.jpg" className="controller"/>
+            </div>
+          </div>
         </div>
-      <div className="SignUp_text">
-        Don't have an account? &nbsp; <Link to="/signup" className="Signup_link"> Sign up</Link>
-      </div>
-      </div>
+      </body>
     </>
   );
 };
