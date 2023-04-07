713808f64d817147e4f6c7db70815b3d215ace34
Shalin06 <jain.75@iitj.ac.in>
2023-04-07 21:11:21 +0530

    Firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase} from "firebase/database"
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,4 +26,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database  = getDatabase(app)
+export const storage = getStorage(app)
 export default app;
